9bcfc1c9352056d671296e14edaaae04dd83a558
Jonathan Hong <jkhong3@hotmail.com>
2014-10-28 21:05:56 -0700

    new pages works

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,19 @@
 class Video < ActiveRecord::Base
+
+	before_create -> do
+	  uid = link.match(YT_LINK_FORMAT)
+	  self.uid = uid[2] if uid && uid[2]
+
+	  if self.uid.to_s.length != 11
+	    self.errors.add(:link, 'is invalid.')
+	    false
+	  elsif Video.where(uid: self.uid).any?
+	    self.errors.add(:link, 'is not unique.')
+	    false
+	  else
+	    get_additional_info
+	  end
+	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :videos, only: [:index, :new, :create]
   root 'videos#index'
+
+  get 'videos/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
